13fa8f4b72e2c153077ce981d1daf33690e47e22
kreddkrikk <kreddkrikk@gmail.com>
2024-04-17 20:27:43 -0500

    update license

--- src/HT1621.h
+++ src/HT1621.h
@@ -1,36 +1,25 @@
 /*******************************************************************************
-Copyright 2016-2018 anxzhu (github.com/anxzhu)
-Copyright 2018-2020 Valerio Nappi (github.com/valerionew) (changes)
-Based on segment-lcd-with-ht1621 from anxzhu (2016-2018)
-(https://github.com/anxzhu/segment-lcd-with-ht1621)
-
-Partially rewritten and extended by Valerio Nappi (github.com/valerionew) in 2018
-
-Rewritten again (github.com/kreddkrikk) with added support for 4-digit displays
-and a focus on low power consumption.
-
-This file is part of the HT1621 arduino library, and thus under the MIT license.
-More info on the project and the license conditions on:
-
-https://github.com/kreddkrikk/ht1621
+Copyright (c) 2016-2018 anxzhu (github.com/anxzhu)
+Copyright (c) 2018 Valerio Nappi (github.com/5N44P) (changes)
+Copyright (c) 2024 kreddkrikk (github.com/kreddkrikk) (changes)
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal in
-the Software without restriction, including without limitation the rights to
-use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
-of the Software, and to permit persons to whom the Software is furnished to do
-so, subject to the following conditions:
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
 
 The above copyright notice and this permission notice shall be included in all
 copies or substantial portions of the Software.
 
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
-FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
-COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
-IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
-CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
 *******************************************************************************/
 
 #ifndef HT1621_H_
